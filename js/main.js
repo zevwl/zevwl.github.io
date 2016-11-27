8dc3b465e7e25b7c466380d882c79bd713af404e
zevwl <zevwl@bhphoto.com>
2016-11-26 22:05:48 -0500

    Added watch feature

--- js/main.js
+++ js/main.js
@@ -1,30 +1,59 @@
 let map;
+let watchEnabled = false;
+let watchId;
+
+let showLocationButton = document.getElementById('show-location');
+let watchLocationButton = document.getElementById('watch-location');
+
+function showMarkerOnCurrentLocation(position) {
+
+    let coords = position.coords;
+    let currentPosition = { lat: coords.latitude, lng: coords.longitude };
+
+    let marker = new google.maps.Marker({
+        position: currentPosition,
+        map: map
+    });
+    map.setCenter(currentPosition);
+}
+
+function showCurrentLocation() {
+    
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showMarkerOnCurrentLocation);
+    } else {
+        console.error('navigator.geolocation is not available ');
+    }
+}
+
+function watchCurrentLocation() {
+
+    if (!watchEnabled) {
+
+         if (navigator.geolocation) {
+            watchId = navigator.geolocation.watchPosition(showMarkerOnCurrentLocation);
+            this.textContent = 'Disable Watch';
+            this.className += 'watch-enabled';
+            watchEnabled = true;
+        } else {
+            console.error('navigator.geolocation is not available ');
+        }
+    } else {
+        // disable watch
+        navigator.geolocation.clearWatch(watchId);
+        watchEnabled = false;
+        this.textContent = 'Watch Current Location';
+        this.className = '';
+    }
+}
 
 function initMap() {
     
-    if (navigator.geolocation) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 41.341090, lng: -74.168289 },
+        zoom: 16
+    });
 
-        navigator.geolocation.getCurrentPosition(function (position) {
-
-            let coords = position.coords;
-            let currentPosition = { lat: coords.latitude, lng: coords.longitude };
-
-            map = new google.maps.Map(document.getElementById('map'), {
-                center: currentPosition,
-                zoom: 16
-            });
-
-            let marker = new google.maps.Marker({
-                position: currentPosition,
-                map: map
-            });
-        });
-
-    } else {
-
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: { lat: 41.341090, lng: -74.168289 },
-            zoom: 16
-        });
-    }
+    showLocationButton.addEventListener('click', showCurrentLocation, false);
+    watchLocationButton.addEventListener('click', watchCurrentLocation, false);
 }
